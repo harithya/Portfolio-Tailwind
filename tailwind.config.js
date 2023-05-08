d626a9ae9e76607ae51460950657545ef4f20d13
Harithya Wisesa <harithya77@gmail.com>
2023-05-08 15:09:45 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,11 @@ module.exports = {
   theme: {
     extend: {
       fontFamilly: {
-        sans: ["Source Sans Pro", "ui-sans-serif", "system-ui"],
-        mono: ["IBM Plex Mono", "ui-monospace"]
+        sans: ['Noto Sans', 'sans-serif'],
       },
       colors: {
-        yellow: {
-          "50": "#ff100109",
-          "100": "#fffdf0",
-          "200": "#fef8be",
-          "300": "#fdf390",
-          "400": "#fded5e",
-          "500": "#fce72c",
-          "600": "#e7d103",
-          "700": "#ab9a02",
-          "800": "#6f6402",
-          "900": "#322d01"
-        }
+        primary: "#443582",
+        'primary-focus': '#33266F'
       }
     },
   },
